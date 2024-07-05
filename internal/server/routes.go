d87efc2322bc1b56fe28c55b0392ee883ea0f234
Anton <87275567+SinnerUfa@users.noreply.github.com>
2024-07-05 21:38:32 +0500

    test

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -15,9 +15,9 @@ func Routes(log *slog.Logger, rep repository.Repository, gzr *gzip.Reader, gzw *
 	// if gzr != nil {
 	// 	r.Use(hundlers.Decompressor(log, gzr))
 	// }
-	// if gzw != nil {
-	// 	r.Use(hundlers.Compressor(log, gzw))
-	// }
+	if gzw != nil {
+		r.Use(hundlers.Compressor(log, gzw))
+	}
 	r.Use(hundlers.Logger(log))
 	r.Route("/", func(r chi.Router) {
 		r.Get("/", hundlers.GetList(log, rep))
