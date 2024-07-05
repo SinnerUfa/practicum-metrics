c0a7bccdae718da19b5c47bf630197912ac50237
Anton <87275567+SinnerUfa@users.noreply.github.com>
2024-07-05 06:19:52 +0500

    Спрнт 2 - Декомпрессор

--- internal/server/hundlers/decompressor.go
+++ internal/server/hundlers/decompressor.go
@@ -2,30 +2,20 @@ package hundlers
 
 import (
 	"compress/gzip"
+	"io"
 	"log/slog"
 	"net/http"
 	"strings"
 )
 
-// type logWriter struct {
-// 	http.ResponseWriter
-// 	status int
-// 	buf    *bytes.Buffer
-// }
+type gzReader struct {
+	io.ReadCloser
+	zr *gzip.Reader
+}
 
-// func (w *logWriter) Header() http.Header {
-// 	return w.ResponseWriter.Header()
-// }
-
-// func (w *logWriter) Write(b []byte) (int, error) {
-// 	mw := io.MultiWriter(w.ResponseWriter, w.buf)
-// 	return mw.Write(b)
-// }
-
-// func (w *logWriter) WriteHeader(statusCode int) {
-// 	w.ResponseWriter.WriteHeader(statusCode)
-// 	w.status = statusCode
-// }
+func (g *gzReader) Read(p []byte) (n int, err error) {
+	return g.zr.Read(p)
+}
 
 func Decompressor(log *slog.Logger, gz *gzip.Reader) func(http.Handler) http.Handler {
 	mid := func(h http.Handler) http.Handler {
@@ -33,7 +23,10 @@ func Decompressor(log *slog.Logger, gz *gzip.Reader) func(http.Handler) http.Han
 			if ct := r.Header.Get("Content-Type"); (ct == "application/json" || ct == "text/html") &&
 				strings.Contains(r.Header.Get("Content-Encoding"), "gzip") {
 				gz.Reset(r.Body)
-				r.Body = gz
+				r.Body = &gzReader{
+					ReadCloser: r.Body,
+					zr:         gz,
+				}
 			}
 			h.ServeHTTP(w, r)
 		}
