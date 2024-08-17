e5bb63eba809606a8093888d10c9ac21df4f7709
Anton <87275567+SinnerUfa@users.noreply.github.com>
2024-08-17 15:42:16 +0500

    1 Добавлены повторы отправки при ошибках 2 В части сервера такие повторы по-моему неуместны, клиент должен при ошибке решать надо или нет делать повтор

--- internal/agent/poster.go
+++ internal/agent/poster.go
@@ -2,6 +2,7 @@ package agent
 
 import (
 	"log/slog"
+	"time"
 
 	repository "github.com/SinnerUfa/practicum-metric/internal/repository"
 	resty "github.com/go-resty/resty/v2"
@@ -18,7 +19,7 @@ func NewPoster(rep repository.Storage, adress string, noBatch bool) *MetricPost 
 	return &MetricPost{rep: rep, adress: adress, noBatch: noBatch}
 }
 
-func (m *MetricPost) Post() {
+func (m *MetricPost) Post() (err error) {
 	l, _ := m.rep.GetList()
 	client := resty.New()
 	if m.noBatch {
@@ -44,8 +45,26 @@ func (m *MetricPost) Post() {
 	}
 	m.counter++
 	slog.Debug("Post metrics", "increment", m.counter)
+	return err
 }
 
 func (m *MetricPost) Tick() {
-	m.Post()
+	var delay int
+
+	for counter := 0; counter < 3; counter++ {
+		err := m.Post()
+		switch counter {
+		case 0:
+			delay = 1
+		case 1:
+			delay = 3
+		case 2:
+			delay = 5
+		}
+		if err != nil {
+			time.Sleep(time.Duration(delay) * time.Second)
+		} else {
+			break
+		}
+	}
 }
